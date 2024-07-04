bc6ca86067a8147f7ca91daeea01cb0de3fdc53e
Olivier Contant <ocontant@cloudoric.com>
2024-07-04 05:13:16 +0400

    Added missing ORM models to modelsImporter.py

--- krakenfx/api/models/account_data/modelsImporter.py
+++ krakenfx/api/models/account_data/modelsImporter.py
@@ -4,11 +4,12 @@ Base = declarative_base()
 
 from krakenfx.api.models.account_data.balanceModel import *
 from krakenfx.api.models.account_data.ledgerModel import *
+from krakenfx.api.models.account_data.openPositionModel import *
 from krakenfx.api.models.account_data.OrderModel import *
-from krakenfx.api.models.account_data.balanceModel import *
 from krakenfx.api.models.account_data.tradeBalanceModel import *
 from krakenfx.api.models.account_data.tradeHistoryModel import *
 from krakenfx.api.models.spot_market_data.assetsPairsModel import *
+from krakenfx.api.models.spot_market_data.ohlcModel import *
 
 __all__ = ['Base',
            'ModelFeeSchedule', 'ModelAssetPairDetails', 'ModelCollateralAssetDetails'
