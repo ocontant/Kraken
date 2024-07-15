e7124a8329cb21398c8fc8dc416d5d706f9a8cc7
Olivier Contant <ocontant@cloudoric.com>
2024-07-16 03:42:27 +0400

    Add pre-commit for alembic
    Ensure models modifications trigger alembic migration script creation.

---
+++ pre_commit_hook.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Get the commit message from the staged commit
+COMMIT_MSG=$(git log -1 --pretty=%B --staged)
+
+# Check if there are any changes in model files
+CHANGED_MODELS=$(git diff --cached --name-only | grep 'krakenfx/repository/models/')
+
+if [ -n "$CHANGED_MODELS" ]; then
+  echo "Detected changes in models. Generating new Alembic migration with message: $COMMIT_MSG"
+  alembic revision --autogenerate -m "$COMMIT_MSG"
+  
+  # Add the new migration file to the commit
+  git add alembic/versions/*
+else
+  echo "No changes in models detected."
+fi
